c77bd515811213680dd95ce6114a5449848756e8
TranGiaPhuc <mtilow2277@gmai.com>
2022-03-11 13:35:08 +0700

    commit

--- frm_ktra/frm_ktra/Form1.cs
+++ frm_ktra/frm_ktra/Form1.cs
@@ -16,5 +16,10 @@ namespace frm_ktra
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
